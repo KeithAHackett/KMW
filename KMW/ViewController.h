6516e1217b7ed2841b09bf025dde690f159614a2
Keith Hackett <KeithAHackett@icloud.com>
2015-03-19 12:52:18 +0000

    Initial Commit

---
+++ KMW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KMW
+//
+//  Created by Keith on 19/03/2015.
+//  Copyright (c) 2015 Keith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KMW/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KMW
+//
+//  Created by Keith on 19/03/2015.
+//  Copyright (c) 2015 Keith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
